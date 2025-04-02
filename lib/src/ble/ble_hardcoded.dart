02649f9e9524e0bad4a9eb828b0a5ac592f46869
Mikołaj Lubiak <lubiak@proton.me>
2025-04-02 12:53:49 +0200

    Change UUIDs

--- lib/src/ble/ble_hardcoded.dart
+++ lib/src/ble/ble_hardcoded.dart
@@ -1,7 +1,7 @@
 import 'package:reactive_ble_platform_interface/src/model/uuid.dart';
 
-final Uuid SERVICE_UUID = Uuid.parse("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+final Uuid SERVICE_UUID = Uuid.parse("3c9a8264-7d7e-41d3-963f-798e23f8b28f");
 final Uuid CHARACTERISTIC_UUID =
-    Uuid.parse("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+    Uuid.parse("59dee772-cb42-417b-82fe-3542909614bb");
 
 final BLE_MTU = 512;
